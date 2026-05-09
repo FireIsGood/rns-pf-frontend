9d844a4f7fc9a46b9e2737a4ac766945cce6f2e3
FireIsGood <fireisadequate@gmail.com>
2026-05-08 18:51:01 -0700

    feat: uncensor lobby text

--- src/lib/stream-api.ts
+++ src/lib/stream-api.ts
@@ -18,61 +18,61 @@ type StreamChunk = {
 };
 
 export async function connectStream(callback: (chunk: StreamChunk) => void) {
-	const stream = await fetch(`${PUBLIC_API_ENDPOINT}/lobbies/stream?format=json`).then(
-		(response) => {
-			const reader = response.body?.getReader();
-			if (reader === undefined) {
-				console.error('Unable to get reader');
-				return;
-			}
-			return new ReadableStream({
-				start(controller) {
-					return pump();
-					function pump(): Promise<Function | undefined> | undefined {
-						return reader?.read().then(({ done, value }) => {
-							if (value) {
-								let id = bytesToInt(value.slice(0, 4));
-								let size = bytesToInt(value.slice(4, 6));
-								let payload = new TextDecoder().decode(value.slice(6, 6 + size));
+	const stream = await fetch(
+		`${PUBLIC_API_ENDPOINT}/lobbies/stream?format=json&uncensored=true`
+	).then((response) => {
+		const reader = response.body?.getReader();
+		if (reader === undefined) {
+			console.error('Unable to get reader');
+			return;
+		}
+		return new ReadableStream({
+			start(controller) {
+				return pump();
+				function pump(): Promise<Function | undefined> | undefined {
+					return reader?.read().then(({ done, value }) => {
+						if (value) {
+							let id = bytesToInt(value.slice(0, 4));
+							let size = bytesToInt(value.slice(4, 6));
+							let payload = new TextDecoder().decode(value.slice(6, 6 + size));
 
-								// Filter heartbeats out
-								if (size !== 0) {
-									let parsedPayload = null;
-									try {
-										parsedPayload = JSON.parse(payload);
-									} catch (error) {
-										console.error(error);
-									}
+							// Filter heartbeats out
+							if (size !== 0) {
+								let parsedPayload = null;
+								try {
+									parsedPayload = JSON.parse(payload);
+								} catch (error) {
+									console.error(error);
+								}
 
-									if (parsedPayload !== null) {
-										const chunk: StreamChunk = {
-											id: id,
-											size: size,
-											data: parsedPayload
-										};
+								if (parsedPayload !== null) {
+									const chunk: StreamChunk = {
+										id: id,
+										size: size,
+										data: parsedPayload
+									};
 
-										callback(chunk);
-									}
+									callback(chunk);
 								}
 							}
+						}
 
-							if (done) {
-								console.log('Connection called a done method');
-								controller.close();
-								return;
-							}
+						if (done) {
+							console.log('Connection called a done method');
+							controller.close();
+							return;
+						}
 
-							controller.enqueue(value);
-							return pump();
-						});
-					}
-				},
-				cancel(reason) {
-					console.log('Connection closed, reason:', reason);
+						controller.enqueue(value);
+						return pump();
+					});
 				}
-			});
-		}
-	);
+			},
+			cancel(reason) {
+				console.log('Connection closed, reason:', reason);
+			}
+		});
+	});
 
 	return stream;
 }
