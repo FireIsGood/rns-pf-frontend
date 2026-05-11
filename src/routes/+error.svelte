fa8fa32e40f11c6f77521991bc4e752514b37e38
FireIsGood <fireisadequate@gmail.com>
2026-05-10 21:24:07 -0700

    feat: 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<section class="hero is-large has-text-centered">
+	<div class="hero-body">
+		<h2 class="title is-1">{page.status}</h2>
+		<h3 class="subtitle">{page.error?.message}</h3>
+		<p><a href="/">Return to home</a></p>
+	</div>
+</section>
